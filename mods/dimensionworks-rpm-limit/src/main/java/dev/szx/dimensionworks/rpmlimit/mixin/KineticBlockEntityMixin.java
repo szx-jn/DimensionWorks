093f9203d5ac765dbf70a6591381cb98350e12ba
mmm <13269935981@163.com>
2026-09-24 10:02:38 +0800

    fix: disable remapping for Create kinetic mixin

--- mods/dimensionworks-rpm-limit/src/main/java/dev/szx/dimensionworks/rpmlimit/mixin/KineticBlockEntityMixin.java
+++ mods/dimensionworks-rpm-limit/src/main/java/dev/szx/dimensionworks/rpmlimit/mixin/KineticBlockEntityMixin.java
@@ -1,3 +1,15 @@
 package dev.szx.dimensionworks.rpmlimit.mixin;
-import com.simibubi.create.content.kinetics.base.KineticBlockEntity; import dev.szx.dimensionworks.rpmlimit.RpmLimitManager; import org.spongepowered.asm.mixin.Mixin; import org.spongepowered.asm.mixin.injection.At; import org.spongepowered.asm.mixin.injection.ModifyVariable;
-@Mixin(KineticBlockEntity.class) public abstract class KineticBlockEntityMixin { @ModifyVariable(method="setSpeed",at=@At("HEAD"),argsOnly=true) private float limit(float speed){return RpmLimitManager.clamp((KineticBlockEntity)(Object)this,speed);} }
+
+import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import dev.szx.dimensionworks.rpmlimit.RpmLimitManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(value = KineticBlockEntity.class, remap = false)
+public abstract class KineticBlockEntityMixin {
+    @ModifyVariable(method = "setSpeed", at = @At("HEAD"), argsOnly = true, remap = false)
+    private float dimensionworks$limitSpeed(float speed) {
+        return RpmLimitManager.clamp((KineticBlockEntity) (Object) this, speed);
+    }
+}
