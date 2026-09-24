57771a0803b05f72fb7ee33a047d91f4373e139d
mmm <13269935981@163.com>
2026-09-24 10:32:25 +0800

    feat: add public KubeJS RPM API

---
+++ mods/dimensionworks-rpm-limit/src/main/java/dev/szx/dimensionworks/rpmlimit/RpmLimitApi.java
@@ -0,0 +1,41 @@
+package dev.szx.dimensionworks.rpmlimit;
+
+import net.minecraft.server.level.ServerPlayer;
+import java.util.UUID;
+
+/** Stable public API for KubeJS and other server-side integrations. */
+public final class RpmLimitApi {
+    private RpmLimitApi() {}
+
+    public static int getLimit(ServerPlayer player) {
+        return RpmLimitManager.getLimit(player.getUUID());
+    }
+
+    public static int getLimit(UUID playerId) {
+        return RpmLimitManager.getLimit(playerId);
+    }
+
+    public static int getLimit(String playerId) {
+        return getLimit(UUID.fromString(playerId));
+    }
+
+    public static void setLimit(ServerPlayer player, int rpm) {
+        RpmLimitManager.setLimit(player, rpm);
+    }
+
+    public static void setLimit(UUID playerId, int rpm) {
+        RpmLimitManager.setLimit(playerId, rpm);
+    }
+
+    public static void setLimit(String playerId, int rpm) {
+        setLimit(UUID.fromString(playerId), rpm);
+    }
+
+    public static int getDefaultLimit() {
+        return RpmLimitConfig.DEFAULT_RPM.get();
+    }
+
+    public static int getMaxLimit() {
+        return RpmLimitConfig.MAX_RPM.get();
+    }
+}
